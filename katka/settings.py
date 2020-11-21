ea6f7f3907487b73c53e61fc798b363e4a74bdbb
CaptainMountain <romych1995@gmail.com>
2020-11-21 18:55:56 +0300

    to static files

--- katka/settings.py
+++ katka/settings.py
@@ -120,10 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "auth_reg/staticfiles"),
-    os.path.join(BASE_DIR, "mainpage/staticfiles"),
-    os.path.join(BASE_DIR, "user_profile/staticfiles"),
+    os.path.join(BASE_DIR, "katka/static"),
 ]
 
--- mainpage/views.py
+++ mainpage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, '../templates/main.html')
+    return render(request, '../templates/mainpage/main.html')
