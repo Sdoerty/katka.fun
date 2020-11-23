3ded0df4ee6ad7665ee4469dad5b15d21a4a2303
CaptainMountain <romych1995@gmail.com>
2020-11-24 00:42:50 +0300

    fix static root

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, '../templates/profile.html')
+    return render(request, '../templates/user_profile/profile.html')
