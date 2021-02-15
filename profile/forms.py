05c4a32807ca64f822ec3fd61ea2c2e656755dfe
CaptainMoutain <romych1995@gmail.com>
2021-02-15 22:47:35 +0300

    useful select activity throught selectField

--- profile/forms.py
+++ profile/forms.py
@@ -7,6 +7,3 @@ class ProfileEditForm(forms.ModelForm):
         model = Profile
         fields = ('avatar', 'city', 'date_of_birth', 'inst', 'vk', 'fb', 'act')
 
-
-def select_activity(self):
-    return [label for value, label in self.fields['act'].choices if value in self['act'].value()]
