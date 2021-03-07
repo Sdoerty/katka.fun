684cb4c1ba0631763988d2e88e07acc6bb0ebf5c
CaptainMoutain <romych1995@gmail.com>
2021-03-07 16:42:09 +0300

    add members1

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from .forms import KatkaForm, KatkaEntryForm
 from .models import Katka
+from django.db import connection
 
 
 def index(request):
@@ -14,15 +15,19 @@ def katka_page(request, pk):
     ktk_item = Katka.objects.get(pk=pk)
 
     if request.method == 'POST':
-        queryset = "UPDATE main_katka SET members = format('%s,%s', members, 100) WHERE id = 13"
-        form = KatkaEntryForm(queryset)
-        if form.is_valid():
-            form.save()
-            return redirect('main')
-    else:
-        form = KatkaForm()
+        # form = Katka.objects.raw("UPDATE main_katka SET members = format('%s,%s', members, 100) WHERE id = 13")
+        # form.save()
+        enter_katka(self=pk)
+        return redirect('main')
 
-    return render(request, 'katka_page/katka_page.html', {"ktk_item": ktk_item, "form": form})
+    return render(request, 'katka_page/katka_page.html', {"ktk_item": ktk_item})
+
+
+def enter_katka(self):
+    with connection.cursor() as cursor:
+        cursor.execute("UPDATE main_katka SET descr=123 WHERE id=13")
+        row = cursor.fetchone()
+    return row
 
 
 def create_katka(request):
