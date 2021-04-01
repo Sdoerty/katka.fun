6efc34615b530fa620835aed584d04d1e915c88f
CaptainMoutain <romych1995@gmail.com>
2021-04-01 23:40:40 +0300

    display personal events in some profile

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from profile.models import Profile
 
 
 def index(request):
@@ -9,4 +10,5 @@ def index(request):
 
 def some(request, pk):
     some_item = User.objects.get(pk=pk)
-    return render(request, 'some/some.html', {"some_item": some_item})
+    prfl = Profile.objects.filter(pk=pk)
+    return render(request, 'some/some.html', {"some_item": some_item, "prfl": prfl})
