fcfb924784657a359135d963b2264dbb1cd3ce73
CaptainMoutain <romych1995@gmail.com>
2021-05-22 19:27:09 +0300

    basic django channels chat like in documentation in katka page

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^table/$', views.index, name='main'),
     url(r'^katka/(?P<pk>\d+)/$', views.katka_page, name='katka'),
-    path('<str:room_name>/', views.room, name='room'),
+    # path('<str:room_name>/', views.room, name='room'),
     url(r'^create_katka/$', views.create_katka, name='create_katka'),
 ]
