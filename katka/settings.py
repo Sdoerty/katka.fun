ec56a48dd511952198575c733aeec1eb1e4f5bb6
romero <CaptainMountain>
2021-05-29 20:43:19 +0300

    rebuild

--- katka/settings.py
+++ katka/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'katka.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'm5',
+        'NAME': 'k1',
         'USER': 'romero',
         'PASSWORD': '2580654',
         'HOST': 'localhost',
